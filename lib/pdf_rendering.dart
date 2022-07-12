112015b83eae1b8f163739c843789d1bc4f88bf7
thuanpm <thuanpm@unicloud.com.vn>
2022-07-12 18:51:40 +0700

    iint data

--- lib/pdf_rendering.dart
+++ lib/pdf_rendering.dart
@@ -1,13 +1,16 @@
-
 import 'dart:async';
+import 'dart:typed_data';
 
 import 'package:flutter/services.dart';
 
 class PdfRendering {
   static const MethodChannel _channel = MethodChannel('pdf_rendering');
 
-  static Future<String?> get platformVersion async {
-    final String? version = await _channel.invokeMethod('getPlatformVersion');
-    return version;
+  static Future initPdfRendering() async {
+    return await _channel.invokeMethod('initPdfRendering');
+  }
+
+  static Future<List<String>> convertPdftoImages(Uint8List data) async {
+    return await _channel.invokeMethod('convertPdftoImages', {'data': data});
   }
 }
